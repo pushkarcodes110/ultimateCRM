0d3e4b35006b415d8ccd2f22ea7bbf4939b2125e
Pushkar <dextercodes110@gmail.com>
2024-03-16 19:50:18 +0530

    'Updated2'

--- tealcrm/urls.py
+++ tealcrm/urls.py
@@ -10,10 +10,10 @@ urlpatterns = [
     path('dashboard/leads/', include('lead.urls')),
     path('dashboard/clients/', include('client.urls')),
     path('dashboard/accounts/', account, name='account'),
-    path('dashboard/team/', include('teams.urls')),
+    path('dashboard/teams/', include('teams.urls')),
     path('dashboard/', include('dashboard.urls')),
     path('about/',about, name='about' ),
-    path('sign-up/', signup, name='signup'),
+    path('sign-up/', signup, name='signup'), 
     path('log-in/', views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('accounts/', include("django.contrib.auth.urls")),
 ]
--- teams/views.py
+++ teams/views.py
@@ -1,11 +1,23 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Team 
+from .forms import TeamForm
 
 # Create your views here.
 @login_required
 def edit_team(request, pk):
     team = get_object_or_404(Team, created_by=request.user, pk=pk)
 
-    context={'team: team'}
-    return render(request, 'team/edit_team.html', context)
+    if request.method=='POST':
+        form = TeamForm(request.POST, instance=team)
+        if form.is_valid():
+            form.save()
+
+            messages.success(request, 'The changes were saved!' )
+            return redirect('account')
+    else:
+        form = TeamForm(instance=team)
+
+    context={'team':team, 'form': form}
+    return render(request, 'teams/edit_team.html', context)
